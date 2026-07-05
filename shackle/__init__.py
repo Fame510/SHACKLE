5e5c8a3839005aced429dec713fe96a63abee88a
Fame510 <docspoc101@gmail.com>
2026-07-05 00:34:21 -0700

    feat(pkg): export decide + canonical_hash from shackle package (the conformance surface is now first-class API)

--- shackle/__init__.py
+++ shackle/__init__.py
@@ -1,4 +1,8 @@
 from .core import Guard, ShackleInterrupt, TriggerEngine, ExecutionState
+from .conformance import decide, canonical_hash
 
-__all__ = ["Guard", "ShackleInterrupt", "TriggerEngine", "ExecutionState"]
+__all__ = ["Guard", "ShackleInterrupt", "TriggerEngine", "ExecutionState",
+    "decide",
+    "canonical_hash",
+]
 __version__ = "0.1.0"
